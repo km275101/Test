684bd1b6bd7b0f8caf3b8d71ffaf7b67dd5cfab2
kammy <kammy@kammy-HP-Pavilion-dv4-Notebook-PC>
2019-02-20 00:42:30 +0530

    Adding factorial

---
+++ Test/src/Test/Factorial.java
@@ -0,0 +1,20 @@
+package Test;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		//int n = sc.nextInt();
+		int n =10;
+		double sum = 1;
+		while(n>0) {
+			sum = sum * n;
+			n--;
+		}
+		System.out.println("Factorial  is : "+sum);
+	}
+
+}
