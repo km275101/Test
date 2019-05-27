834e0a9e894c4ed2721bd1894bcba5a48f9f0a7f
kamlesh.maurya <kamlesh.maurya@PDC2A-9J0N6C2.dir.svc.accenture.com>
2019-05-27 17:11:15 +0530

    pattern 7

---
+++ Test/src/Pattern/Pattern7.java
@@ -0,0 +1,54 @@
+package Pattern;
+
+import java.util.Scanner;
+
+public class Pattern7 {
+
+	/**
+	 * Pattern5 ------------------
+	  	 *
+	    ***
+	   *****
+	    ***
+	     *
+	     
+	 * -----------------------
+	 */
+
+	public static void main(String[] args) {
+		int n , i, j, space = 1;
+		System.out.println("Enter the number : ");
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		
+		space = n - 1;
+        for (j = 1; j <= n; j++) 
+        {
+            for (i = 1; i <= space; i++) 
+            {
+                System.out.print(" ");
+            }
+            space--;
+            for (i = 1; i <= 2 * j - 1; i++) 
+            {
+                System.out.print("*");                
+            }
+            System.out.println("");
+        }
+        space = 1;
+        for (j = 1; j <= n - 1; j++) 
+        {
+            for (i = 1; i <= space; i++) 
+            {
+                System.out.print(" ");
+            }
+            space++;
+            for (i = 1; i <= 2 * (n - j) - 1; i++) 
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+	}
+
+}
