cec5456054c20bdc0866fc5ca1672fd0ab8c2120
kamlesh.maurya <kamlesh.maurya@PNL-1H883H2.jadeglobal.com>
2019-09-10 22:02:15 +0530

    special character test

---
+++ Test/src/Test/$Test.java
@@ -0,0 +1,24 @@
+package Test;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+public class $Test {
+	
+	
+
+	public static void main(String[] args) {
+		Map<String, String> m = new HashMap<String, String>();
+		m.put("name", "AAA");
+		m.put("address", "Pune");
+		m.put("langauge", "English");
+		
+		m.put(null, "EEE");
+		m.put(null, "BBB");
+		
+		System.out.println(m);
+		
+	}
+
+}
