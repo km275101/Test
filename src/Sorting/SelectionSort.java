c6725c08447f817cf8f69eb33f6582c464b03b0b
kamlesh.maurya <kamlesh.maurya@PNL-1H883H2.jadeglobal.com>
2020-04-11 23:57:57 +0530

    Adding changes

--- src/Sorting/SelectionSort.java
+++ src/Sorting/SelectionSort.java
@@ -1,5 +1,7 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class SelectionSort {
 	
 	static void sort(int arr[]) 
@@ -30,11 +32,54 @@ public class SelectionSort {
         System.out.println(); 
     } 
   
+    static void selectionSort(int arr[]) {
+    	int size = arr.length;
+    	int min ;
+    	for(int i =0 ; i<size; i++) {
+    		min = i;
+    		for(int j =i+1; j<size; j++) {
+    			if(arr[min] > arr[j]) {
+    				min = j;
+    			}
+    		}
+    		int temp = arr[i];
+    		arr[i] = arr[min];
+    		arr[min] = temp;
+     	}
+    	
+    	
+    }
+	
 
-	public static void main(String[] args) {
+    static void selectionSort1(int arr[], int x) {
+    	int count = 0;
+    	int size = arr.length;
+    	int min ;
+    	for(int i =0 ; i<size; i++) {
+    		if(x == count) {
+    			printArray(arr);
+    		}
+    		count++;
+    		min = i;
+    		for(int j =i+1; j<size; j++) {
+    			if(arr[min] > arr[j]) {
+    				min = j;
+    			}
+    		}
+    		int temp = arr[i];
+    		arr[i] = arr[min];
+    		arr[min] = temp;
+     	}
+    	
+    	
+    }
+	
+    public static void main(String[] args) {
 		
 		int arr[] = {64,25,12,22,11}; 
-        sort(arr); 
+        //sort(arr); 
+		selectionSort1(arr,5);
+		
         System.out.println("Sorted array"); 
         printArray(arr); 
 	}
