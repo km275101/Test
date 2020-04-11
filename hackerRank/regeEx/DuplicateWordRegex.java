bb493f8e6333b506b2d3fc20b2c4cdd9e534e547
kamlesh.maurya <kamlesh.maurya@PNL-1H883H2.jadeglobal.com>
2020-04-12 00:24:29 +0530

    Done changes

---
+++ hackerRank/regeEx/DuplicateWordRegex.java
@@ -0,0 +1,10 @@
+package regeEx;
+
+public class DuplicateWordRegex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- hackerRank/regeEx/UserNameRegexCheck.java
+++ hackerRank/regeEx/UserNameRegexCheck.java
@@ -1,4 +1,4 @@
-package string;
+package regeEx;
 
 import java.util.Scanner;
 
