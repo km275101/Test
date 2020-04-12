4b09112f331bdf15bfd50c029c5851fb33e93009
kamlesh <kamlesh.maurya>
2020-04-13 01:29:14 +0530

    Adding changes

--- hackerRank/regeEx/DuplicateWordRegex.java
+++ hackerRank/regeEx/DuplicateWordRegex.java
@@ -1,10 +1,37 @@
 package regeEx;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class DuplicateWordRegex {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 String regex = "\\b([a-z]+)\\b(?:\\s+\\1\\b)+";
+	        Pattern p = Pattern.compile(regex , Pattern.CASE_INSENSITIVE);
+
+	        Scanner in = new Scanner(System.in);
+	        int numSentences = Integer.parseInt(in.nextLine());
+	        
+	        while (numSentences-- > 0) {
+	            String input = in.nextLine();
+	            System.out.println(input.toString());
+	            Matcher m = p.matcher(input);
+	            
+	            // Check for subsequences of input that match the compiled pattern
+	            while (m.find()) {
+	            	
+	                input = input.replaceAll(m.group(), m.group(1));
+	            	
+	            }
+	            
+	            // Prints the modified sentence.
+
+	           System.out.println(input);
+	        }
+	        
+	        in.close();
+	    }
 
 	}
 
-}
